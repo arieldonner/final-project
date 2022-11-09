25a58f055bd5599218cc7ed82d4978f776b1261c
arieldonner <arieldonner@gmail.com>
2022-11-09 19:39:30 +0000

    Completed nav bar for desktop and mobile

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,16 +1,74 @@
 import React from 'react';
 
 export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    if (event.target === event.currentTarget) {
+      this.setState({ isOpen: !this.state.isOpen });
+    }
+  }
+
   render() {
-    return (
-      <nav className='light-blue container-fluid nav-container'>
-        <div className='p-1 white row align-items-center'>
-          <div className='col-1 cookie brand'>IceTime</div>
-          <div className='col-1'>Events</div>
-          <div className='col-1'>Outfits</div>
-          <div className='col-1'>About</div>
-        </div>
-      </nav>
-    );
+    if (this.state.isOpen === false) {
+      return (
+        <nav className='container-fluid nav-container'>
+          <div className='white row align-items-center'>
+            <div className='col-1 cookie brand'>IceTime</div>
+            <div className='col-1 d-none d-md-block'>
+              <a href='#' className='nav-links p-0'>Events</a>
+            </div>
+            <div className='col-1 d-none d-md-block'>
+              <a href='#' className='nav-links p-0'>Outfits</a>
+            </div>
+            <div className='col-1 d-none d-md-block'>
+              <a href='#' className='nav-links p-0'>About</a>
+            </div>
+            <div className='col-7 d-none d-md-block text-end logout'>
+              <a href='#' className='nav-links p-0'>
+                Log Out
+                <i className="fa-regular fa-circle-user ps-2" />
+              </a>
+            </div>
+            <div className='col-9 .d-none .d-sm-block .d-md-none d-lg-none d-xl-none text-end logout'>
+              <i className="fa-solid fa-bars" onClick={this.handleClick} />
+            </div>
+          </div>
+        </nav>
+      );
+    }
+    if (this.state.isOpen === true) {
+      return (
+        <nav className='container-fluid nav-container-tall'>
+          <div className='white align-items-center'>
+            <div className='row align-items-center'>
+              <div className='cookie brand-mobile col-10 ps-4'>IceTime</div>
+              <div className='col-2 ps-4'>
+                <i className="fa-solid fa-xmark" onClick={this.handleClick} />
+              </div>
+            </div>
+            <div className='col-1 ps-3'>
+              <a href='#' className='nav-links p-0'>Events</a>
+            </div>
+            <div className='col-1 ps-3'>
+              <a href='#' className='nav-links p-0'>Outfits</a>
+            </div>
+            <div className='col-1 ps-3'>
+              <a href='#' className='nav-links p-0'>About</a>
+            </div>
+            <div className='col ps-3'>
+              <a href='#' className='nav-links p-0'>
+                Log Out
+                <i className="fa-regular fa-circle-user ps-2" />
+              </a>
+            </div>
+          </div>
+        </nav>
+      );
+    }
   }
 }
