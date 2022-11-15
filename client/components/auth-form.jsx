13c6d287019b3adf2bb76f8545413e7cdffe7fd5
arieldonner <arieldonner@gmail.com>
2022-11-15 19:38:02 +0000

    Fixed styling for sign-in/register page

--- client/components/auth-form.jsx
+++ client/components/auth-form.jsx
@@ -7,35 +7,36 @@ export default class AuthForm extends React.Component {
       username: '',
       password: ''
     };
-    // this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // handleChange(event) {
-  //   const { name, value } = event.target;
-  //   this.setState({ [name]: value });
-  // }
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    console.log(this.state);
+  }
 
-  // handleSubmit(event) {
-  //   event.preventDefault();
-  //   const { action } = this.props;
-  //   const req = {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify(this.state)
-  //   };
-  //   fetch(`/api/auth/${action}`, req)
-  //     .then(res => res.json())
-  //     .then(result => {
-  //       if (action === 'sign-up') {
-  //         window.location.hash = 'sign-in';
-  //       } else if (result.user && result.token) {
-  //         this.props.onSignIn(result);
-  //       }
-  //     });
-  // }
+  handleSubmit(event) {
+    event.preventDefault();
+    const { action } = this.props;
+    const req = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    };
+    fetch(`/api/auth/${action}`, req)
+      .then(res => res.json())
+      .then(result => {
+        if (action === 'sign-up') {
+          window.location.hash = 'sign-in';
+        } else if (result.user && result.token) {
+          this.props.onSignIn(result);
+        }
+      });
+  }
 
   render() {
     const { action } = this.props;
@@ -52,12 +53,12 @@ export default class AuthForm extends React.Component {
     const submitButtonText = action === 'sign-up'
       ? 'Create Account'
       : 'Login';
-
+    console.log(action);
     return (
-      <div className='form-style container-fluid col-5 p-4'>
-        <h1 className='cookie blue text-center'>{welcomeMessage}</h1>
-        <form>
-          <div className='mb-3'>
+      <div className='form-style container-fluid col-10 col-md-5 p-4 mt-5'>
+        <h1 className='cookie blue text-center mb-3'>{welcomeMessage}</h1>
+        <form onSubmit={handleSubmit}>
+          <div className='mb-4'>
             <label htmlFor='username' className='form-label'>Username</label>
             <input
             required
@@ -65,21 +66,23 @@ export default class AuthForm extends React.Component {
             id='username'
             type="text"
             name="username"
+            onChange={handleChange}
             className="form-control" />
           </div>
-          <div className='mb-3'>
+          <div className='mb-4'>
             <label htmlFor='password' className='form-label'>Password</label>
             <input
             required
             id='password'
             type="password"
             name="password"
+            onChange={handleChange}
             className="form-control" />
           </div>
-          <div className='d-flex justify-content-center'>
-            <button type='submit' className='btn btn-primary'>{submitButtonText}</button>
+          <div className='d-flex justify-content-center mb-4'>
+            <button type='submit' className='btn btn-primary col-12'>{submitButtonText}</button>
           </div>
-          <div className='d-flex justify-content-center'>
+          <div className='d-flex justify-content-center mb-2'>
             <a className='text-muted' href={alternateActionHref}>{alternateActionText}</a>
           </div>
         </form>
--- client/pages/auth.jsx
+++ client/pages/auth.jsx
@@ -6,7 +6,6 @@ import { appContext } from '../lib';
 
 export default class AuthPage extends React.Component {
   render() {
-
     const { user, route, handleSignIn } = this.context;
 
     if (user) return <Redirect to="" />;
