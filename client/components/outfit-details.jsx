a4c409580aaea3793a4a39fd56d87abb03f471a2
arieldonner <arieldonner@gmail.com>
2022-12-12 21:46:47 +0000

    Added navbar to outfit-details page

--- client/components/outfit-details.jsx
+++ client/components/outfit-details.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NotFound from './not-found';
+import Navbar from './navbar';
 
 export default class OutfitDetails extends React.Component {
   constructor(props) {
@@ -40,50 +41,53 @@ export default class OutfitDetails extends React.Component {
 
   render() {
     return (
-      <div className="container-fluid">
-        <div className='d-flex justify-content-center align-items-center col-12'>
-          <a href='#outfits'><i className="fa-solid fa-chevron-left blue" /></a>
-          <h1 className='heading cookie col-10 col-md-8 col-lg-5'>My Outfit</h1>
-        </div>
-        {this.state.error === true &&
-          <NotFound />
-        }
-        {this.state.loading === true &&
-          <div className='d-flex justify-content-center'>
-            <div className="lds-default"><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /></div>
+      <div>
+        <Navbar />
+        <div className="container-fluid">
+          <div className='d-flex justify-content-center align-items-center col-12'>
+            <a href='#outfits'><i className="fa-solid fa-chevron-left blue" /></a>
+            <h1 className='heading cookie col-10 col-md-8 col-lg-5'>My Outfit</h1>
           </div>
-        }
-        <div className='container-fluid col-12 col-md-9 col-lg-6 p-4 form-style'>
-          <a href='#edit-outfit' className='red fs-5 text-decoration-none float-end'>Edit</a>
-          <div className='d-flex justify-content-center col-12'>
-            <div className='big-circle mb-2'>
-              <img src={this.state.outfitImg} alt='Outft image' className='outfit-img' />
+          {this.state.error === true &&
+            <NotFound />
+          }
+          {this.state.loading === true &&
+            <div className='d-flex justify-content-center'>
+              <div className="lds-default"><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /></div>
             </div>
-          </div>
-          <div>
-            <h2 className='blue pb-2'>{this.state.outfitName}</h2>
-          </div>
-          <div className='d-flex'>
-            {this.state.category === 'Synchro' &&
-              <span className='my-badge pink-back'>{this.state.category}</span>
-            }
-            {this.state.category === 'Single' &&
-              <span className='my-badge blue-back'>{this.state.category}</span>
-            }
-            {this.state.category === 'Pairs' &&
-              <span className='my-badge pale-back'>{this.state.category}</span>
-            }
-            {this.state.category === 'Dance' &&
-              <span className='my-badge purple-back'>{this.state.category}</span>
-            }
-          </div>
-          <div>
-            <h5 className='mt-3 mb-0'>Bottoms:</h5>
-            <p className='blue pb-2'>{this.state.bottoms}</p>
-          </div>
-          <div>
-            <h5 className='mt-3 mb-0'>Hair/Makeup:</h5>
-            <p className='blue pb-2'>{this.state.makeup}</p>
+          }
+          <div className='container-fluid col-12 col-md-9 col-lg-6 p-4 form-style'>
+            <a href='#edit-outfit' className='red fs-5 text-decoration-none float-end'>Edit</a>
+            <div className='d-flex justify-content-center col-12'>
+              <div className='big-circle mb-2'>
+                <img src={this.state.outfitImg} alt='Outft image' className='outfit-img' />
+              </div>
+            </div>
+            <div>
+              <h2 className='blue pb-2'>{this.state.outfitName}</h2>
+            </div>
+            <div className='d-flex'>
+              {this.state.category === 'Synchro' &&
+                <span className='my-badge pink-back'>{this.state.category}</span>
+              }
+              {this.state.category === 'Single' &&
+                <span className='my-badge blue-back'>{this.state.category}</span>
+              }
+              {this.state.category === 'Pairs' &&
+                <span className='my-badge pale-back'>{this.state.category}</span>
+              }
+              {this.state.category === 'Dance' &&
+                <span className='my-badge purple-back'>{this.state.category}</span>
+              }
+            </div>
+            <div>
+              <h5 className='mt-3 mb-0'>Bottoms:</h5>
+              <p className='blue pb-2'>{this.state.bottoms}</p>
+            </div>
+            <div>
+              <h5 className='mt-3 mb-0'>Hair/Makeup:</h5>
+              <p className='blue pb-2'>{this.state.makeup}</p>
+            </div>
           </div>
         </div>
       </div>
