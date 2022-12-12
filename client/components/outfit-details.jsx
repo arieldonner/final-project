4abca6f129c9e1a6dd681e1c0ef418b5d65c2d49
arieldonner <arieldonner@gmail.com>
2022-12-12 18:57:51 +0000

    Started adding the heading and tile for outfit-details component

--- client/components/outfit-details.jsx
+++ client/components/outfit-details.jsx
@@ -22,7 +22,15 @@ export default class OutfitDetails extends React.Component {
 
   render() {
     return (
-      <div>Hi</div>
+      <div className="container-fluid">
+        <div className='d-flex justify-content-center align-items-center col-12'>
+          <a href='#outfits'><i className="fa-solid fa-chevron-left blue" /></a>
+          <h1 className='heading cookie'>My Outfit</h1>
+        </div>
+        <div className='container-fluid col-12 col-md-9 col-lg-6 p-4 form-style'>
+          <p>Test</p>
+        </div>
+      </div>
     );
   }
 }
