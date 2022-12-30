7f84ab067e99231c25287146f12c4d4f13afbfaa
arieldonner <arieldonner@gmail.com>
2022-12-30 18:21:07 +0000

    Added clickable icon to go back to the calendar page and new+ button to create a new event from the upcoming page

--- client/pages/upcoming.jsx
+++ client/pages/upcoming.jsx
@@ -8,6 +8,10 @@ export default class Upcoming extends React.Component {
         <Navbar />
         <div className="container-fluid">
           <h1 className="heading cookie">Upcoming Events</h1>
+          <div className='d-flex align-items-center justify-content-center'>
+            <a href='#'><i className="fa-solid fa-calendar-days" /></a>
+            <a href='#create-event' className='btn btn-primary col-3 col-lg-2 offset-4 offset-md-1'>New+</a>
+          </div>
         </div>
       </div>
     );
